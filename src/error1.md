620847a1597d7e9b1059b04e9e0a5cafba68493b
user1 <hideyoshi.hideyo.19620703@gmail.com>
2024-05-10 20:44:08 +0900

    make mydocker.sh (help)

---
+++ src/error1.md
@@ -0,0 +1,2 @@
+## Error I got
+I got error when I ran `${SDE_INSTALL}/bin/veth_setup.sh` removed `set -e` in the code.
--- src/mydocker.sh
+++ src/mydocker.sh
@@ -1,8 +1,34 @@
 #!bin/bash
-
-set -ue
+#set -u
 ## get the container id
-dhash=`docker ps -a | grep "970-2" | awk '{print $1}'`
+function print_help() { ## Output help message
+    echo "USAGE: bash $(basename ""$0"") -i <container number>"
+    echo "1. Check the container number."
+    echo "  `docker ps -a`"
+    echo "2. Which container you want to start and count from the top."
+    echo "3. Run \"bash $(basename ""$0"") -i 1\" if you want to start the top container."
+    exit 0
+}
+trap 'exit' ERR
+
+CID="1"     ## default container id
+HELP=false  ## default help message
+
+## parse
+for i in "$@"; do
+    case $i in
+        --) shift; break;;
+        -h) HELP=true; shift 1;;
+        -i) CID=$2; shift 2;;
+    esac
+done
+
+if [ $HELP == true ]; then
+    print_help
+fi
+
+dhash=`docker ps -a | grep "970" | awk '{print $1}'`
+dhash=` echo $dhash| awk -v val="$CID" '{print $val}'`
 docker start ${dhash}
 docker exec -it ${dhash} bash
 #docker exec -it ${dhash} bash -c "bash --rcfile mybash.sh"
