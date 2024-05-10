5c0c3ace51709b7b531295afe565c4fca55ff01e
user1 <hideyoshi.hideyo.19620703@gmail.com>
2024-05-10 23:15:31 +0900

    add run3.sh

--- src/run2.sh
+++ src/run2.sh
@@ -1,3 +1,7 @@
 # 1. bash mydocker.sh
 # 2. sudo -s
 # 3. bash run2.sh
+
+source .env
+${SDE}/run_switchd.sh -p switch_tofino2_y1 --arch Tofino2
+
---
+++ src/run3.sh
@@ -0,0 +1,4 @@
+./build.sh -p angel_eye -u switch
+cd ${SDE}
+. ./tools/set_sde.bash
+bash ./tools/p4_build.sh ./tools/shared/switch_tofino2_y1.p4 --with-tofino2
