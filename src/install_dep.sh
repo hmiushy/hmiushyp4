aafad8ec938c5b768514fd66e70de08aa0813686
hmiushy <hideyoshi.hideyo.19620703@gmail.com>
2024-05-13 21:55:35 +0900

    change dep

--- src/install_dep.sh
+++ src/install_dep.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-#HOMEUSER="/home/user1"
+#HOMEUSER="${HOME}"
 HOMEUSER=${HOME}
 OSNAME="Ubuntu"
 OSVERS="20.04"
 JOBS=5
-SDEINSTALL=${HOMEUSER}/bf-sde-9.7.0/install
+SDE_INSTALL=${HOME}/bf-sde-9.7.0/install
 KEYWORD="apt-get"
 WITHPROTO="yes"
 
 #bash
 ## install boost
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_boost.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_boost.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
 ## install grpc and protobuf
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_grpc.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_grpc.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
 ## install thrift
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_thrift.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_thrift.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
 ## install bridge_utils
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_bridge_utils.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_bridge_utils.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
 ## install libcli
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_libcli.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_libcli.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
 ## install PI
-env python3 /home/user1/bf-sde-9.7.0/p4studio/dependencies/source/install_pi.py --os-name Ubuntu --os-version 20.04 --jobs 5 --sde-install /home/user1/bf-sde-9.7.0/install --keyword apt-get --with-proto yes
+env python3 ${HOME}/bf-sde-9.7.0/p4studio/dependencies/source/install_pi.py --os-name ${OSNAME} --os-version ${OSVERS} --jobs ${JOBS} --sde-install ${SDE_INSTALL} --keyword ${KEYWORD} --with-proto ${WITHPROTO}
 
